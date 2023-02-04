ae09d51398450006708cbc956c5556a32cc90a51
nahianreza <91044737+nahianreza@users.noreply.github.com>
2023-02-04 04:01:33 -0500

    Added Authentication

--- src/App.js
+++ src/App.js
@@ -1,23 +1,103 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import fire from './fire';
+import Login from './components/Login';
+import Home from './components/Home';
 import './App.css';
 
 function App() {
+
+  const[user, setUser] = useState('');
+  const[email, setEmail] = useState('');
+  const[password, setPassword] = useState('');
+  const[emailError, setEmailError] = useState('');
+  const[passwordError, setPasswordError] = useState('');
+  const[hasAccount, setHasAccount] = useState(false);
+
+  const handleLogin = () => {
+    clearErrors();
+    fire
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch((err) => {
+        switch (err.code) {
+          case "auth/invalid-email":
+          case "auth/user-disabled":
+          case "auth/user-not-found":
+            setEmailError(err.message);
+            break;
+          case "auth/wrong-password":
+            setPasswordError(err.message);
+            break;
+        }
+      });
+  };
+
+  const handleSignUp = () => {
+    clearErrors();
+    fire
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch((err) => {
+        switch (err.code) {
+          case "auth/email-already-in-use":
+          case "auth/invalid-email":
+            setEmailError(err.message);
+            break;
+          case "auth/weak-password":
+            setPasswordError(err.message);
+            break;
+        }
+      });
+  };
+
+  const handleLogout = () => {
+    fire.auth().signOut();
+  };
+
+  const authListener = () => {
+    fire.auth().onAuthStateChanged((user) => {
+      if(user) {
+        clearInputs();
+        setUser(user);
+      } else {
+        setUser("");
+      }
+    });
+  };
+
+  useEffect(() => {
+    authListener();
+  }, []);
+
+  const clearInputs = () => {
+    setEmail('');
+    setPassword('');
+  }
+
+  const clearErrors = () => {
+    setEmailError('');
+    setPasswordError('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {user ? (
+        <Home handleLogout = {handleLogout} />
+      ): (
+        <Login 
+          email = {email}
+          setEmail = {setEmail}
+          password= {password}
+          setPassword = {setPassword}
+          hasAccount = {hasAccount}
+          setHasAccount = {setHasAccount}
+          handleLogin = {handleLogin}
+          handleSignUp = {handleSignUp}
+          emailError = {emailError}
+          passwordError = {passwordError}  
+        />
+      )}
+      
     </div>
   );
 }
---
+++ src/fire.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDJNPbCtZj0AaX7GmizaNqcqz4vCEtsl5w",
+    authDomain: "tech-incubator-d2208.firebaseapp.com",
+    projectId: "tech-incubator-d2208",
+    storageBucket: "tech-incubator-d2208.appspot.com",
+    messagingSenderId: "239734403426",
+    appId: "1:239734403426:web:583a5b7956485fd03cfc3b"
+  };
+
+const fire = firebase.initializeApp(firebaseConfig);
+
+
+
+export default fire;
